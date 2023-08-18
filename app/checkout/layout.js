5a3ce214f5f3200d9de9cb5b95e3e5993776bb56
Osama shahbaz <oshahbaz74@gmail.com>
2023-08-18 22:11:04 +0500

    fix

--- app/checkout/layout.js
+++
@@ -1,7 +0,0 @@
-export const metadata = {
-  title: 'Check out page',
-  description: 'this is login page of my website',
-};
-export default function checkOutLayout({ children }) {
-  return <div>{children}</div>;
-}
